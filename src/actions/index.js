0073bc20347074fffcf1cdcdc5d3b6fe8f212649
Subhadarsini-10 <71827873+Subhadarsini-10@users.noreply.github.com>
2021-06-13 19:38:38 +0530

    changing the state to add favourite as well

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,17 @@
-{
-    type: 'ADD_MOVIES'
+//{
+//    type: 'ADD_MOVIES'
+//}
+//{/*{
+//    type: 'DE CREASE_COUNT'
+//}*/}
+
+//action type
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+//action creator
+export function addMovies (movies){
+    return{
+        type: ADD_MOVIES,
+        movies
+    }
 }
-{/*{
-    type: 'DE CREASE_COUNT'
-}*/}
